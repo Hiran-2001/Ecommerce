dbd8a2f99877911c5623ad716554f869632c1ac9
Hiran Raj <hirankvlr@gmail.com>
2022-09-29 19:26:09 +0530

    modified

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,11 +2,13 @@ import React from 'react'
 import './Header.css'
 import Logo from '../../assets/logo.png'
 import SearchArea from './SearchArea'
+import Profile from './Profile'
 function Header() {
   return (
     <div id='header-main'>
             <img id='logo' src={Logo} alt="" />
             <SearchArea/>
+            <Profile/>
     </div>
   )
 }
