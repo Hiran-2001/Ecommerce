5341b00207d6b7a0442915915bd9c88b019bba06
Hiran Raj <hirankvlr@gmail.com>
2022-09-29 18:17:15 +0530

    modified

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Header.css'
 import Logo from '../../assets/logo.png'
+import SearchArea from './SearchArea'
 function Header() {
   return (
     <div id='header-main'>
             <img id='logo' src={Logo} alt="" />
+            <SearchArea/>
     </div>
   )
 }
---
+++ src/Components/Header/SearchArea.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./SearchArea.css";
+function SearchArea() {
+  return (
+    <div id="SearchArea-div">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default SearchArea;
