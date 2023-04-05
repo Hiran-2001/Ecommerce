e0c495509f745e3f21944146fb93988ca3f74d59
Hiran Raj <hirankvlr@gmail.com>
2023-04-05 11:19:30 +0530

    working on navbar

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,75 +1,82 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Search } from "@mui/icons-material"
-
+import React from "react";
+import styled from "styled-components";
+import { Search } from "@mui/icons-material";
+import { Badge } from "@mui/material";
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 const Container = styled.div`
-  height:60px;
-  ${'' /* background-color:black; */}
-`
+  height: 60px;
+  ${"" /* background-color:black; */}
+`;
 const Wrapper = styled.div`
- padding:10px 20px;
- display :flex;
- justify-content:space-between;
-`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+`;
 
 const Left = styled.div`
-flex:1;
-display:flex;
-`
+  flex: 1;
+  display: flex;
+`;
 const Center = styled.div`
-flex:1;
-`
-
+  flex: 1;
+  text-align: center;
+`;
+const Logo = styled.div`
+  font-weight: bold;
+`;
 const Right = styled.div`
-flex:1;
-`
+  flex: 1;
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+`;
 
 const Language = styled.span`
-font-size:14px;
-cursor:pointer;
-`
-const SearchContainer = styled.div`
-border:solid .3px gray;
-display:flex;
-align-items:center ;
-padding:5px;
-margin-left:25px ;
+  font-size: 14px;
+  cursor: pointer;
+`;
 
-`
+const SearchContainer = styled.div`
+  border: solid 0.3px gray;
+  display: flex;
+  align-items: center;
+  padding: 5px;
+  margin-left: 25px;
+`;
 
 const Input = styled.input`
-border:none ;
-outline:none
-
-`
-
-
-
-
-
+  border: none;
+  outline: none;
+`;
 
 function Navbar() {
-    return (
-        <Container>
-            <Wrapper>
-                <Left>
-                    <Language>
-                        EN
-                    </Language>
-                    <SearchContainer>
-                        <Input />
-                        <Search />
-                    </SearchContainer>
-                </Left>
-                <Center>
-                    center
-                </Center>
-                <Right>
-                    Right
-                </Right>
-            </Wrapper>
-        </Container>
-    )
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input />
+            <Search />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>RcR</Logo>
+        </Center>
+        <Right>
+          <MenuItem>Register</MenuItem>
+          <MenuItem>Sign ip</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlinedIcon/>
+            </Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
 }
 
-export default Navbar
+export default Navbar;
